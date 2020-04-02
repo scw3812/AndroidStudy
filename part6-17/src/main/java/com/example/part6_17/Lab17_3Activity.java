4c69f334c774580f63d7e2671848a46c789613c5
scw3812 <dlffpraos@naver.com>
2020-04-02 23:31:51 +0900

    part 6-17 RecyclerView

---
+++ part6-17/src/main/java/com/example/part6_17/Lab17_3Activity.java
@@ -0,0 +1,14 @@
+package com.example.part6_17;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lab17_3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab17_3);
+    }
+}
